7c7e7d0f07c390ecc0c4c6f223bb87b26598286c
benjithompson <mail@benjithompson.com>
2017-02-19 13:03:10 -0800

    updates to write process

--- writedata.py
+++ writedata.py
@@ -3,7 +3,6 @@ import sys
 import os
 import datetime
 
-
 def write_subreddit_text(posts):
     """iterates through all posts and comments and writes them to specified file"""
     try:
@@ -16,23 +15,6 @@ def write_subreddit_text(posts):
             print('aborting file write...')
     except OSError as err:
         print(err.strerror)
-        raise
-
-def __write_data(filename, posts):
-    time = str(datetime.datetime.now())
-    print('saving to ' + filename + '...')
-    print('Time started: ' + time)
-    target = open(filename, 'w')
-    target.write(time)
-    for post in posts:
-        target.write(post.title)
-        post.comments.replace_more(limit=0)
-        for comment in post.comments.list():
-            target.write(comment.body)
-    time = str(datetime.datetime.now())
-    target.write(time)
-    print('Complete!')
-    print(time)
 
 def verify_write(filename):
     """Return true if user wants to overwrite existing file"""
@@ -42,3 +24,33 @@ def verify_write(filename):
     else:
         ans = input('Do you really want to write to ' + filename + '(y/n)?: ')
         return ans == 'y' or ans == 'Y'
+
+def __write_data(filename, posts):
+    data = ''
+    time = str(datetime.datetime.now())
+    print('saving to ' + filename + '...')
+    print('Time started: ' + time)
+    target = open(filename, 'w')
+    target.write(time + '\n')
+    target.flush()
+    sys.stdout.flush()
+
+
+    #TODO: catch HTTP errors and using settings file
+    for post in posts:
+        title = str(post.title.encode('ascii', 'ignore'))
+        data += title
+        post.comments.replace_more(limit=0)
+        for comment in post.comments.list():
+            comment = str(comment.body.encode('ascii', 'ignore'))
+            data += comment
+            print('.', end='')
+            sys.stdout.flush()
+        target.flush()
+
+    time = str(datetime.datetime.now())
+    data = ''.join(time)
+    print('Compiling data complete. Writing to file...')
+    target.write(data)
+    target.flush()
+    print('Complete!')
