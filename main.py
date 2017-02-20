e576f8e5e1d2c19b17658cb644e3357649385f6f
Ben Thompson <thomp211@wwu.edu>
2017-02-20 15:59:59 -0800

    fixed main filename error

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import reddit as r
 import writedata as wd
 import sys
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
-
 def main(argv):
-    filename = argv[1]
+    filename = ''
+    if len(argv) == 2:
+        filename = argv[1]
     REDDIT = r.get_reddit()
     POSTS = r.get_subreddit_posts(REDDIT, 'The_Donald')
     wd.write_subreddit_text(POSTS, filename)
     
+if __name__ == "__main__":
+    main(sys.argv)
