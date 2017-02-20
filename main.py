2a7cd7600dd1cb07fe10e75b35e9e6e2acd07310
Ben Thompson <thomp211@wwu.edu>
2017-02-20 13:52:17 -0800

    added main method

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 
 import reddit as r
 import writedata as wd
+import sys
 
-REDDIT = r.get_reddit()
-POSTS = r.get_subreddit_posts(REDDIT, 'The_Donald')
-wd.write_subreddit_text(POSTS)
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+def main(argv):
+    filename = argv[1]
+    REDDIT = r.get_reddit()
+    POSTS = r.get_subreddit_posts(REDDIT, 'The_Donald')
+    wd.write_subreddit_text(POSTS, filename)
+    
