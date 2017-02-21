795e7f7cd48aea45f8d82ac1d017992ee51538d2
Ben Thompson <thomp211@wwu.edu>
2017-02-20 21:14:23 -0800

    added main

---
+++ runanalysis.py
@@ -0,0 +1,17 @@
+from analysis import analysis
+from utils import path
+import pprint as p
+
+def main():
+
+    filename = input('Input filename: ')
+    output = input('Output filename: ')
+    filepath = path.get_path(filename, '/data/', '.data')
+    wordcount = analysis.getdict(filepath)
+    list = analysis.getsortedkv(wordcount)
+    output = path.get_path(output, '/data/', '.txt')
+    with open(output, 'w') as f:
+        p.pprint(list, f)
+
+if __name__ == '__main__':
+    main()
